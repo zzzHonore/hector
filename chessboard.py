3cff761d65916dcc0459c86282d764159f69f567
W-Nelissen <wne@sint-pieterscollege.be>
2024-01-20 16:36:01 +0100

    simplify structure

---
+++ chessboard.py
@@ -0,0 +1,28 @@
+import pygame
+import chess
+
+   
+class ChessBoard(object):
+    def __init__(self,top,left,width):
+        self.left=left
+        self.top=top
+        self.width=width
+
+    def draw_background(self,window):
+        """Draws the background of the chess board"""
+        # This shows how to draw a rectangle on the screen
+        color="grey"
+        rectangle = pygame.Rect(self.left,self.top,self.width,self.width)
+        pygame.draw.rect(window,color,rectangle)
+        # verander deze code zodat je in plaats van 1 rect in 1 kleur, 64 rects in de juiste kleuren tekent
+
+
+        
+    def draw_pieces(self,window,position):
+        """Draws the pieces on the board"""
+        # Hier tonen we hoe je een foto van een schaakstuk op het scherm zet
+        piece = pygame.image.load("./assets/K-black.png")
+        window.blit(piece,(self.left,self.top))
+        # verandere deze code zodat je in plaats van 1 schaakstuk, alle juiste schaakstukken op het scherm zet
+        # probeer dit eerst met eender welke positie van het bord
+        # zorg daarna dat je rekening houdt met de positie die meegegeven is in het argument "position"
--- main.py
+++ main.py
@@ -1,57 +1,42 @@
+# general imports
 import pygame
 import chess
+# import our own files
+from constants import WINDOW_WIDTH, WINDOW_HEIGHT
+import chessboard
 
 # set up pygame
 pygame.init()
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Hector Chess - Sint-Pieterscollege, Jette")
 clock = pygame.time.Clock()
 running = True
 
-to_draw = []
+# set up our own chessboard
+board = chessboard.ChessBoard(top=100,left=100,width=400)
 
-def create_grid(position=(WINDOW_WIDTH//2,WINDOW_HEIGHT//2),width=400):
-    """
-    This function creates a grid of rects to serve as a background for all the pieces.
-    Accepts
-        position (tuple): x and y coords of the middle of the grid
-        width (int): the width of the grid
-    Returns
-        grid_rects (list): list with pygame rects that represent a grid when drawn
-    """
-    grid_rects = []
-    for x in range(0,width,width//8):
-        for y in range(0,width,width//8):
-            grid_rects.append(pygame.Rect(x+position[0]-width//2,y+position[1]-width//2,width/8,width/8))
-    return grid_rects
+
 
 # game loop
 while running:
-    # ... events for player interaction
+    # ... get events for player interaction
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONUP:
             mouse_position = pygame.mouse.get_pos()
-            to_draw.append(mouse_position)
 
-    # ... logic
 
-    # clear the screen before rendering
-    window.fill("black")
+    # ... logic comes here
+
+
     # ... rendering the screen
+    # clear the screen before rendering
+    window.fill("white")
 
-    # the background
-    grid = create_grid()
-    colors = ["gray","white"]
-    color_counter = 1
-    for i in range(len(grid)):
-        pygame.draw.rect(window,colors[color_counter%2],grid[i])
-        if not (i+1)%8 == 0:
-            color_counter += 1
-
+    # draw chessboard
+    board.draw_background(window)
+    board.draw_pieces(window,position=chess.STARTING_FEN)
 
     pygame.display.flip() # update the screen
 
