93420206128df1b1cec7a8dcca36d83d03685b50
Robin-Berger <156842764+Robin-Berger@users.noreply.github.com>
2024-01-22 20:07:37 +0100

    Update tutorial_pygame-de volgende.py

--- tutorials/tutorial_pygame-de volgende.py
+++ tutorials/tutorial_pygame-de volgende.py
@@ -1,26 +1,21 @@
 """
 Om te kunnen tekenen op een window hebben we een externe library of package nodig.
+Wij kiezen voor PYGAME (je zal pygame eerst moeten installeren voor je deze tutorial doet)
 PYGAME zorgt voor:
-    Het openen van een window
-    Tekenen van lijnen en figuren
-    Tekenen van images
-    Opvangen van user-events zoals muisedown, mousemove,...
-"""
+    -Het openen van een window met bepaalde grootte en titel(caption)
+    -Tekenen van lijnen en figuren (al dan niet opgevuld)
+    -Opladen, herschalen en tekenen van images (bmp,png,jpeg,...)
+    -Opvangen van user-events zoals mousedown, mousemove,...
 
+Tutorial-pygame-deel-3: Tekenen van opgevulde objecten met rand
+"""
 
 import pygame as pg
 pg.init()
 win = pg.display.set_mode((800, 600))
 pg.display.set_caption("Hector Chess - Sint-Pieterscollege, Jette")
 
-# Om dingen te tekenen spreken we de klasse :draw: van pygame aan. Deze heeft tekenroutines
-# Eerste parameter is telkens het venster :win:
-# Tweede parameter is telkens een kleur :color:
-# Derde parameter is telkens een iets met coordinaten :rect: of :pt:, naargelang het soort figuur
-# Teken een rechthoek met dikte 3
-
-# Eerst bepalen we de kleuren die we gebruiken
-# Constanten altijd in hoofdletters
+# Onze kleuren
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -29,7 +24,23 @@ PINK = (255, 0, 255)
 CYAN = (0, 255, 255)
 BURLYWOOD = (222, 184, 135)
 CHOCOLATE = (210, 105, 30)
-# We bepalen de rechthoeken die we nodig hebben
+
+# Overzicht van de gebruikte functies (zie Tutorial Deel 2)
+#    pg.draw.rect(surface, color, rect, width=0)
+#    pg.draw.circle(surface, color, center, radius, width=0)
+# Door 1 maal de figuren te tekenen met width=0 krijgen we een opgevulde figuur
+# Door dan 1 maal de figuren te tekenen met width=2 krijgen we een rand
+# Eerst tekenen we een rode rechthoek met groene rand
+rect1 = pg.Rect((40, 280, 200, 75))  # (left,top,width,height)
+pg.draw.rect(win, RED, rect1,0)  # Eerst de rode achtergrond
+pg.draw.rect(win, GREEN, rect1,4)  # Dan de groenekader
+# Dan tekenen we een witte cirkel met straal 45, met blauwe rand
+pg.draw.circle(win, WHITE, (rect1.right, rect1.bottom), 45, 0)
+pg.draw.circle(win, BLUE, (rect1.right, rect1.bottom), 45, 3)
+
+
+
+
 # Als we pg.Rect gebruiken om een rechthoek te definieren kunnen we ook rect.top, rect.width,... gebruiken
 # De functie pg.Rect zet de python rect (40, 80, 200, 75) om naar een pg.Rect
 rect1 = pg.Rect((40, 80, 200, 75))  # (left,top,width,height)
