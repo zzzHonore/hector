30de8a2383396566d3838f098c1556a3aad803a6
Robin-Berger <156842764+Robin-Berger@users.noreply.github.com>
2024-01-22 20:14:13 +0100

    Update tutorial_pygame-end.py

--- tutorials/tutorial_pygame-end.py
+++ tutorials/tutorial_pygame-end.py
@@ -10,28 +10,12 @@ PYGAME zorgt voor:
 Tutorial-pygame-deel-4: 
 """
 
-# stap 1: importeer pygame. Je kan nu de functies van pygame gebruiken
 import pygame as pg
-
-# stap 2: roep pg.init() op.
-# Doe je dit niet dan zijn er functies van pygame die niet correct werken
 pg.init()
-
-# Eerst openen we een venster met pygame
-# De variabele win zal je moeten meegeven met elke routine die iets op het window moet tekenen
-# we spreken hiervoor de pygame klasse :display: aan
 win = pg.display.set_mode((800, 600))
-
-# We geven het venster een gepaste titel
 pg.display.set_caption("Hector Chess - Sint-Pieterscollege, Jette")
 
-# Om dingen te tekenen spreken we de klasse :draw: van pygame aan. Deze heeft tekenroutines
-# Eerste parameter is telkens het venster :win:
-# Tweede parameter is telkens een kleur :color:
-# Derde parameter is telkens een iets met coordinaten :rect: of :pt:, naargelang het soort figuur
-# Teken een rechthoek met dikte 3
-
-# Eerst bepalen we de kleuren die we gebruiken
+# Voordat we beginnen met tekenen bepalen we de kleuren die we gaan gebruiken
 # Constanten altijd in hoofdletters
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -41,14 +25,8 @@ PINK = (255, 0, 255)
 CYAN = (0, 255, 255)
 BURLYWOOD = (222, 184, 135)
 CHOCOLATE = (210, 105, 30)
-# We bepalen de rechthoeken die we nodig hebben
-# Als we pg.Rect gebruiken om een rechthoek te definieren kunnen we ook rect.top, rect.width,... gebruiken
-# De functie pg.Rect zet de python rect (40, 80, 200, 75) om naar een pg.Rect
-rect1 = pg.Rect((40, 80, 200, 75))  # (left,top,width,height)
-pg.draw.rect(win, RED, rect1,4)
-pg.draw.line(win, GREEN, (rect1.left, rect1.top), (rect1.right, rect1.bottom))
-# We tekenen een cirkel met straal 45, rond de rechteronderhoek van de rectangle, met dikte 3
-pg.draw.circle(win, BLUE, (rect1.right, rect1.bottom), 45, 3)
+
+#Nog te verwerken, nu eten!
 
 # We doen nog eens hetzelfde met een andere rect, maar nu zetten we de lijndikte gelijk aan 0
 rect1 = pg.Rect((340, 80, 200, 75))  # (left,top,width,height)
@@ -87,7 +65,7 @@ win.set_clip((0, 0, win.get_width(), win.get_height()))
 pg.draw.rect(win, BURLYWOOD, (clip_rect.left-10,clip_rect.top-10,10,200),0)  # Eerst de achtergrond
 
 # Uitsmijter: het window object van pygame kan ook een :image: tonen, via blit functie
-queen_image = pg.image.load("assets/Q-white.png")
+queen_image = pg.image.load("assets/pieces/W_Queen.png")
 rect1 = pg.Rect((100, 440, 64, 64))  # (left,top,width,height)
 pg.draw.rect(win, CHOCOLATE, rect1,0)  # Eerst de achtergrond
 win.blit(queen_image, rect1)
